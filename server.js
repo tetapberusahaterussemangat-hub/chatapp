ce67b544c97b61e2fe858cf99cf09564c71a994d
tetapberusahaterussemangat-hub <tetapberusahaterussemangat@gmail.com>
2026-04-28 06:43:10 +0700

    fix route

--- server.js
+++ server.js
@@ -50,6 +50,16 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server jalan di http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+// 🔥 ini yang penting (biar HTML kebaca)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// optional (biar aman kalau route lain)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log("Server jalan");
 });
