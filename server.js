01f45a5a7feaec147d5a70cfb4fbe1961970b9c8
tetapberusahaterussemangat-hub <tetapberusahaterussemangat@gmail.com>
2026-04-28 12:53:54 +0700

    fix route

--- server.js
+++ server.js
@@ -59,3 +59,11 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.send('Server jalan di Render');
 });
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
